e5b575ec6726870d85261147bb512f4956884f45
Andrew Alfred <andrewalfred99@yahoo.com>
2021-06-19 19:09:51 +0200

    Add project files.

---
+++ Thesis Management/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Thesis_Management.MvcApplication" Language="C#" %>
---
+++ Thesis Management/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Thesis_Management.Startup))]
+namespace Thesis_Management
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
